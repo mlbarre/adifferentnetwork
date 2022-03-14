08a10b2c936fed55bfb607b1d037678209432ba1
mlbarre <mlbarrera1538@gmail.com>
2022-03-13 20:03:29 -0500

    modified index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,4 @@
 const router = require('express').Router();
-const htmlRoutes = require('./html/html-routes');
-
-router.use('/', htmlRoutes);
 
 router.use((req, res) => {
     res.status(404).send('<h1>😝 404 Error!</h1>');
