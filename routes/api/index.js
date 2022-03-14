2b48fa24817ee4a429494ee7098a0469f5be0c73
mlbarre <mlbarrera1538@gmail.com>
2022-03-13 20:00:47 -0500

    added starting routes w/ router

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+// const userRoutes = require('./user_routes.js');
+// const thoughtsRoutes = require('./thought_routes.js');
+
+module.exports = router;
