246dc82a35c24a0d1b1c6bf3e94d867e50a7fcc5
mlbarre <mlbarrera1538@gmail.com>
2022-03-13 19:44:28 -0500

    added reaction schema in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat.js');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 128
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    { 
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 const ThoughtSchema = new Schema(
     {
@@ -16,6 +44,7 @@ const ThoughtSchema = new Schema(
         },
         username: {
             type: String,
+            required: true
         },
         reactions: [ReactionSchema]
     },
