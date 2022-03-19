d518baf6f7f522aa9838a67a9ab45e45b775a31c
mlbarre <mlbarrera1538@gmail.com>
2022-03-18 19:15:19 -0500

    added user routes

--- routes/api/user_routes.js
+++ routes/api/user_routes.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 
 const {
     getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
 } = require('../../controllers/users-controller.js');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
